3af0f995c57e3f7c56710050224f238ae40e7ae1
NandorKovacs <nandor1kovacs@gmail.com>
2022-07-11 13:50:48 +0200

    efficient sonar

--- 2021/1/harder_sonar_efficient.cc
+++ 2021/1/harder_sonar_efficient.cc
@@ -1,16 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <deque>
 
 int main() {
   int a = 0, b = 0;;
 
-
+  std::deque<int> dq;
 
   for (int i = 0; i < 3; ++i) {
     int x;
     std::cin >> x;
+    dq.push_back(x);
     a += x;
   }
-  b = a;
+  int x;
+  int res = 0;
+  while (std::cin >> x) {
+    int b = a - dq.front() + x;
+    if (b > a) {
+      ++res;
+    }
+    a = b;
+    dq.pop_front();
+    dq.push_back(x);
+  }
 
+  std::cout << res << std::endl;
 }
